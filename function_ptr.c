78cfcbc60f4a2b48af225eb7dd4e28f6554a774e
xinajun zheng <710403758@qq.com>
2019-03-06 09:15:15 +0800

    pointer to function array of c lang

---
+++ function_ptr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int func(void){ printf("func\n");}
+
+int func1(void){ printf("func1\n");}
+
+int main(int argc, const char *argv[])
+{
+	void *a[2] = {func,func1};
+
+	void (*(*a_ptr)[2])(void) = &a;
+
+	for(int i = 0; i < sizeof(a)/sizeof(a[0]);i++)
+	{
+		((void (*)(void))(*(a+i)))();
+	}
+	return 0;
+}
